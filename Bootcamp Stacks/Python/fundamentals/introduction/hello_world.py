2683788010e724497cfaaa0b1138202835688f30
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-02-07 06:40:33 -0600

    Hello World Practice Complete

--- Bootcamp Stacks/Python/fundamentals/introduction/hello_world.py
+++ Bootcamp Stacks/Python/fundamentals/introduction/hello_world.py
@@ -11,6 +11,5 @@ print('Hello World ' + str(42))  # with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-fave_food2 = "pizza"
-print("I love to eat {} and {}.").format(fave_food1,fave_food2)
-print(f"I love to eat {fave_food1} and {pfave_food}.")  # with an f string
+print("I love to eat {} and {}.".format(fave_food1,fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}.")  # with an f string
