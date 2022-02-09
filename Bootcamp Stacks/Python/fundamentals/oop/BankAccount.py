080075dcacf80c22efe7f194aeeeda233096d97b
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-02-09 13:04:10 -0600

    BankAccount Completed

--- Bootcamp Stacks/Python/fundamentals/oop/BankAccount.py
+++ Bootcamp Stacks/Python/fundamentals/oop/BankAccount.py
@@ -3,17 +3,27 @@ class BankAccount:
         self.int_rate = int_rate
         self.balance = balance
     def deposit(self, amount):
-        balance += amount
-    def withdraw(self, amount):
-        if balance < 0:
-            balance -= 5
+        self.balance += amount
+        return self
+    def withdraw(self, amount): #Wanted to add added complexity. This account will only charage the 5 dollar fee if the withdrawl amount is greater than 5 dollars. 
+        if amount > 5 and amount > self.balance:
+            self.balance -= 5
             print('Insufficient funds: Charging a $5 fee')
-             balance -= amount
+        if amount < 5 and amount > self.balance: 
+            self.balance -= amount
+        else:
+            self.balance -= amount
+        return self
     def display_account_info(self):
-        print(f'Balance:$ {balance}')
+        print(f'Balance:$ {self.balance}')
+        return self
     def yield_interest(self):
-        if balance > 0:
-            balance += (balance * int_rate)
+        if self.balance > 0:
+            self.balance += (self.balance * self.int_rate)
+        return self
 
-Account_0101 = BankAccount(.21,200)
-account_102 = BankAccount(.05,1000)
+account_0101 = BankAccount(.21,200)
+account_0102 = BankAccount(.05,1000)
+
+account_0101.deposit(200).deposit(500).deposit(2).withdraw(5000)
+account_0102.deposit(150).deposit(50).withdraw(200).withdraw(5).withdraw(200).withdraw(1).yield_interest().display_account_info()
