90cac09ccaa0a88756a7d6c561042e93f093c10b
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-02-09 17:05:43 -0600

    Dojo Pets - Framework w/ Pass Stastements

---
+++ Bootcamp Stacks/Python/fundamentals/oop/dojo_pets.py
@@ -0,0 +1,27 @@
+class Ninja:
+    def __init__(self,first_name,last_name,treats,pet_food,pet):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.treat = treats
+        self.pet_food = pet_food
+        self.pet = pet
+    def walk():
+        pass
+    def feed():
+        pass
+    def bathe():
+        pass 
+
+class Pet:
+    def __init__(self, name, type, tricks):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+    def sleep():
+        pass
+    def eat():
+        pass
+    def play():
+        pass
+    def noise():
+        pass 
