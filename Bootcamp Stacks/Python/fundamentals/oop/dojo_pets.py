e00b894f59479b2ba35086cc00c0120bc772f071
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-02-10 04:30:11 -0600

    modularizing practice

---
+++ Bootcamp Stacks/Python/fundamentals/oop/dojo_pets.py
@@ -0,0 +1,8 @@
+from dojo_pets_classes import Ninja
+from dojo_pets_classes import Health
+from dojo_pets_classes import Bird
+from dojo_pets_classes import Pet
+from dojo_pets_classes import Dog
+from dojo_pets_classes import Cat
+
+Nina = Ninja('Terry','Brooks',True,True,)
--- Bootcamp Stacks/Python/fundamentals/oop/dojo_pets_classes.py
+++ Bootcamp Stacks/Python/fundamentals/oop/dojo_pets_classes.py
@@ -1,10 +1,10 @@
 class Ninja:
-    def __init__(self,first_name,last_name,treats,pet_food):
+    def __init__(self,first_name,last_name,treats,pet_food,pet):
         self.first_name = first_name
         self.last_name = last_name
         self.treat = treats
         self.pet_food =  pet_food
-        self.pet = Pet()
+        self.pet = Pet(self.)
     def walk(self):
         self.Pet.play(self)
         return self
