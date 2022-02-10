40c119c501589a22e48dc0cd150c369d48525a2b
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-02-10 14:33:09 -0600

    Understanding Routes Complete

--- BootcampStacks/Python/flask/fundamentals/understanding_routing/server.py
+++ BootcampStacks/Python/flask/fundamentals/understanding_routing/server.py
@@ -8,7 +8,15 @@ app = Flask(__name__)
 def hello_world():
     for name in names:
         random_name = names[randrange(51)]
-    print(
-        f'Welcome to the World Wide Web, I am your Web Server, {random_name}')
-    user_name = input('What is your Name?')
-    print(f'Great to meet you {user_name}, What am I serving up')
+    print(f'Welcome to the World Wide Web, I am your Web Server, {random_name} Great to meet you, What am I serving up')
+@app.route('/dojo')
+def dojo():
+    print('Dojo')
+
+@app.route('/say/<name>')
+def personal_greeting(name):
+    print(f'g\'day mate! I hear your name is {name} ')
+
+@app.route('/repeat/<num_of_iterations>/<echo_word>')
+def parroting(num_of_iterations,echo_word):
+    print(echo_word * int(num_of_iterations))
