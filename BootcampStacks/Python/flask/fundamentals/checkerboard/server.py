c9e764916b9cc737bd47a72adf56dc2ebc03dfd7
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-02-12 09:22:21 -0600

    add virtual env

--- BootcampStacks/Python/flask/fundamentals/checkerboard/server.py
+++ BootcampStacks/Python/flask/fundamentals/checkerboard/server.py
@@ -7,7 +7,4 @@ def home(side1, side2):
     return render_template('index.html',side1=side1,side2=side2)
 
 if __name__ == '__main__':
-
-
-#DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000,debug=True)
