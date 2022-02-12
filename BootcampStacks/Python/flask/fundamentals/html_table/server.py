ba275b6ad068f6aff2dd14032fba88f85e86e902
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-02-12 12:31:50 -0600

    html-table w/ server. Needs CSS and HTML with loop

--- BootcampStacks/Python/flask/fundamentals/html_table/server.py
+++ BootcampStacks/Python/flask/fundamentals/html_table/server.py
@@ -1,16 +1,16 @@
 from flask import Flask,redirect,url_for,render_template,request
-from random import random
+from json import dumps
 
-
-
-@app.route('/')
+app=Flask(__name__)
+@app.route('/',methods=['GET','POST'])
 def render_table():
-    users= [
-{'first_name': 'Michael', 'last_name': 'Choi'},
-{'first_name': 'John', 'last_name': 'Supsupin'},
-{'first_name': 'Mark', 'last_name': 'Guillen'},
-{'first_name': 'KB', 'last_name': 'Tonel'}
-]
+    users = [
+        {'first_name': 'Michael', 'last_name': 'Choi'},
+        {'first_name': 'John', 'last_name': 'Supsupin'},
+        {'first_name': 'Mark', 'last_name': 'Guillen'},
+        {'first_name': 'KB', 'last_name': 'Tonel'}
+    ]
+    return users
 
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
