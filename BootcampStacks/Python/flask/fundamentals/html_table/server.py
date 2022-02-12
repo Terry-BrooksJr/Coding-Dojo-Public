cdbdc66f81c25a967d4d91e02ac43dba57b76572
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-02-12 11:56:19 -0600

    set-up project folders and virtual env

--- BootcampStacks/Python/flask/fundamentals/html_table/server.py
+++ BootcampStacks/Python/flask/fundamentals/html_table/server.py
@@ -1,19 +1,17 @@
 from flask import Flask,redirect,url_for,render_template,request
 from random import random
 
-\]
 
-@app.route('/lists')
-def render_lists():
-    # Soon enough, we'll get data from a database, but for now, we're hard coding data
-    student_info = [
-        {'name': 'Michael', 'age': 35},
-        {'name': 'John', 'age': 30},
-        {'name': 'Mark', 'age': 25},
-        {'name': 'KB', 'age': 27}
-    ]
-    randomList = random.sample(range(10, 30), 20)
-    return render_template("lists.html", random_numbers=randomList, students=student_info)
+
+@app.route('/')
+def render_table():
+    users= [
+{'first_name': 'Michael', 'last_name': 'Choi'},
+{'first_name': 'John', 'last_name': 'Supsupin'},
+{'first_name': 'Mark', 'last_name': 'Guillen'},
+{'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
 if __name__ == '__main__':
     #DEBUG is SET to TRUE. CHANGE FOR PROD
     app.run(port=5000,debug=True)
