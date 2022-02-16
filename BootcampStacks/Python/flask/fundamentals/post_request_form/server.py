0cbc88d8a697ab053f97c433c5d4be4bbc3a1dac
Terry Brooks, Jr <Terry.Arthur@BrooksJr.com>
2022-02-16 06:40:55 -0600

    Catching Up

--- BootcampStacks/Python/flask/fundamentals/post_request_form/server.py
+++ BootcampStacks/Python/flask/fundamentals/post_request_form/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, jswonify, render_template, request, redirectfrom datetime import datetime
+from flask import Flask, jsonify, render_template, request, redirect
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -12,10 +13,10 @@ def index():
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    form_response = jsonify(age = request.form['age'], name = request.form['name'])
-    file_write_date = datetime.now()
-    filename = form_response[1]+file_write_date
-    with open(file) as file:
+    form_response = jsonify(age = request.form['age'], name = request.form['name']);
+    file_write_date = datetime.now();
+    filename = f'{form_response[1]}{file_write_date}.json';
+    submission = open(f'./{filename}', a+);
     return redirect('/confirmation')
 
 
